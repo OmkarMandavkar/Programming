20d3d80815e359ad5c61ecb305bfda3204040406
Omkar Mandavkar <73053306+OmkarMandavkar@users.noreply.github.com>
2024-08-23 19:27:56 +0530

    Update P26.java

--- src/basics/P26.java
+++ src/basics/P26.java
@@ -2,10 +2,35 @@
 
 package basics;
 
+import java.util.Scanner;
+
 public class P26 {
 
 	public static void main(String[] args) {
 
-	}
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 
+		int sum = 0;
+		int temp = n;
+
+		int count = (int) (Math.log10(n) + 1);
+
+		while (n != 0) {
+			int digit = n % 10;
+			int prod = 1;
+
+			for (int i = 1; i <= count; i++) {
+				prod = prod * digit;
+			}
+			sum = sum + prod;
+			n = n / 10;
+		}
+
+		if (sum == temp) {
+			System.out.println("Armstrong Number");
+		} else {
+			System.out.println("Not an Armstrong Number");
+		}
+	}
 }
