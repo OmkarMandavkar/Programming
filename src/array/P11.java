a87652b3ac9e731bc277033ca92f341b85a1a648
Omkar Mandavkar <73053306+OmkarMandavkar@users.noreply.github.com>
2024-08-29 12:23:13 +0530

    Update P11.java

--- src/array/P11.java
+++ src/array/P11.java
@@ -1,3 +1,25 @@
+/*
+ Array Operations: Total number of decrement operation performed
+ You are given an array A of size N and an integer K.
+ You have to make all the elements of the array divisible by K.
+ You can decrease any element of the array by 1 in one operation.
+ 
+ Find the total number of decrement operations required to make all the elements of the array divisible by K.
+ 
+ Input: 5 6 -> denotes N & K
+ 		5 9 12 16 25 -> denotes N elements
+ 	
+ Output: 13
+ 
+ Explanation: The array is [5, 9, 12, 25, 16] and K = 6;
+ 1. To make 5 divisible by 6 we will subtract 5 from it. 
+ 	Therefore 5-5 = 0 which is divisible by 6. Therefore ans = 5.
+ 2. To make 9 divisible by 6 we will subtract 3 from it. 
+ 	Therefore 9-3 = 6 which is divisible by 6. Therefore ans = 5+3.
+ 3. Likewise the final ans will be ans = 5+3+0+4+1 = 13
+ 
+ */
+
 package array;
 
 public class P11 {
